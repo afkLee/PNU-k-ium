b69a0aca9d337b48d935019afe2b41612a25e871
EungJae Lee <84191599+afkLee@users.noreply.github.com>
2022-10-06 15:16:51 +0900

    데이터 분할 저장

--- UntitledR.R
+++ UntitledR.R
@@ -3,8 +3,8 @@
 #라이브러리 로딩 
 
 
-# 파일위치는 각자 변경!
-data<-read.csv("/Users/leeeungjae/Downloads/TrainSet.csv" , header= T)
+# 파일읽기 (위치는 각자 알아서 변경)
+data<-read.csv("/Users/leeeungjae/Downloads/TrainSet.csv" , header= T, encoding = "UTF-8")
 
 
 head(data)
@@ -14,3 +14,16 @@ View(data)
 
 
 summary(data)
+
+#class의 이름을 보여준다.
+colnames(data)
+
+
+#class별로 데이터들 저장
+Findingsdata <- Corpus(VectorSource(data[,"Findings"]))
+Conclusiondata <- Corpus(VectorSource(data[,"Conclusion."]))
+AcuteInfarctiondata <- Corpus(VectorSource(data[,"AcuteInfarction"]))
+
+
+
+
